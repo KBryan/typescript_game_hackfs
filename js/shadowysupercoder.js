ba15d96fd8709afcb713e498791b202d8bac1c25
Kwame Bryan <kwame.bryan@gmail.com>
2021-08-02 22:20:40 -0400

    Added debug plotting for Jump Tables

--- js/shadowysupercoder.js
+++ js/shadowysupercoder.js
@@ -45,13 +45,37 @@ var ShadowySuperCoder;
 })(ShadowySuperCoder || (ShadowySuperCoder = {}));
 var Generator;
 (function (Generator) {
+    var Jump = /** @class */ (function () {
+        function Jump() {
+            this.offsetY = 0;
+            this.offsetX = 0;
+        }
+        Jump.prototype.toString = function () {
+            return "offsetX: " + this.offsetX + ", offsetY: " + this.offsetY;
+        };
+        return Jump;
+    }());
+    Generator.Jump = Jump;
+})(Generator || (Generator = {}));
+var Generator;
+(function (Generator) {
     var JumpTables = /** @class */ (function () {
+        // -------------------------------------------------------------------------
         function JumpTables() {
-            // velocites
-            this._jumpVelocites = [];
+            // velocities
+            this._jumpVelocities = [];
+            // list of possible jumps for each jump velocity and position within cell
+            this._jumpDefs = [];
+            // results of jump table analysis
+            this._jumpOffsetsY = [];
+            this._jumpOffsetYMax = 0;
+            this._jumpOffsetXMins = {};
+            this._jumpOffsetXMaxs = {};
             this.calculateJumpVelocities();
+            this.calculateJumpTables();
         }
         Object.defineProperty(JumpTables, "instance", {
+            // -------------------------------------------------------------------------
             get: function () {
                 if (JumpTables._instance === null) {
                     JumpTables._instance = new JumpTables();
@@ -61,29 +85,207 @@ var Generator;
             enumerable: false,
             configurable: true
         });
+        // -------------------------------------------------------------------------
         JumpTables.prototype.calculateJumpVelocities = function () {
             // all height samples
             for (var i = 0; i <= Generator.Parameters.HEIGHT_STEPS; i++) {
-                // maximun height of jump for this step
+                // maximum height of jump for this step
                 var height = Generator.Parameters.HEIGHT_MIN + (Generator.Parameters.HEIGHT_MAX - Generator.Parameters.HEIGHT_MIN) / Generator.Parameters.HEIGHT_STEPS * i;
-                // v - sqrt(-(2 * s * g))
-                this._jumpVelocites[i] = -Math.sqrt(2 * height * Generator.Parameters.GRAVITY);
+                // v = sqrt(-(2 * s * g))
+                this._jumpVelocities[i] = -Math.sqrt(2 * height * Generator.Parameters.GRAVITY);
             }
         };
         Object.defineProperty(JumpTables.prototype, "minJumpVelocity", {
+            // -------------------------------------------------------------------------
             get: function () {
-                return this._jumpVelocites[0];
+                return this._jumpVelocities[0];
             },
             enumerable: false,
             configurable: true
         });
         Object.defineProperty(JumpTables.prototype, "maxJumpVelocity", {
+            // -------------------------------------------------------------------------
             get: function () {
-                return this._jumpVelocites[this._jumpVelocites.length - 1];
+                return this._jumpVelocities[this._jumpVelocities.length - 1];
             },
             enumerable: false,
             configurable: true
         });
+        // -------------------------------------------------------------------------
+        // ---------------------------- JUMP TABLES --------------------------------
+        // -------------------------------------------------------------------------
+        JumpTables.prototype.calculateJumpTables = function () {
+            // all jump velocities
+            for (var height = 0; height <= Generator.Parameters.HEIGHT_STEPS; height++) {
+                this._jumpDefs[height] = [];
+                // step from left to right on cell
+                for (var step = 0; step < 1 /*Parameters.CELL_STEPS*/; step++) {
+                    this.calculateJumpCurve(step, height);
+                }
+            }
+            // analyze created jump tables
+            this.analyzeJumpTables();
+        };
+        // -------------------------------------------------------------------------
+        JumpTables.prototype.calculateJumpCurve = function (step, jumpIndex) {
+            // simulation timestep
+            var timeStep = 1 / 60;
+            // take jump velocity we calculated previously
+            var velocity = this._jumpVelocities[jumpIndex];
+            // start at middle of first step to spread samples better over cell
+            // x and y positions are in pixels
+            var x = step * Generator.Parameters.CELL_SIZE / Generator.Parameters.CELL_STEPS
+                + Generator.Parameters.CELL_SIZE / Generator.Parameters.CELL_STEPS / 2;
+            var y = 0;
+            // y position in cells coordinates (row within grid)
+            var cellY = 0;
+            // help variables to track previous position
+            var prevX, prevY;
+            // array of jumps from starting position to possible destinations
+            var jumpDefs = [];
+            // helper object that will help us keep track of visited cells
+            var visitedList = {};
+            // half of player body width
+            var playerWidthHalf = Generator.Parameters.PLAYER_BODY_WIDTH / 2 * 0.5;
+            // debug
+            var debugBitmap = (JumpTables._DEBUG) ? JumpTables.debugBitmapData : null;
+            // offset drawing of curve little bit down (by 4 cells),
+            // otherwise it will be cut at top as we start jump at point [x, 0]
+            var yOffset = Generator.Parameters.CELL_SIZE * 4;
+            // simulate physics
+            while (cellY < Generator.Parameters.GRID_HEIGHT) {
+                // save previous position
+                prevX = x;
+                prevY = y;
+                // adjust velocity
+                velocity += Generator.Parameters.GRAVITY * timeStep;
+                // new posiiton
+                y += velocity * timeStep;
+                x += Generator.Parameters.VELOCITY_X * timeStep;
+                // draw path - small white dot
+                if (JumpTables._DEBUG) {
+                    debugBitmap.rect(x, y + yOffset, 2, 2, "#FFFFFF");
+                }
+                // left and right bottom point based on body width.
+                var leftCell = void 0, rightCell = void 0;
+                cellY = Math.floor(y / Generator.Parameters.CELL_SIZE);
+                // falling down
+                if (velocity > 0) {
+                    // crossed cell border to next vertical cell?
+                    if (cellY > Math.floor(prevY / Generator.Parameters.CELL_SIZE)) {
+                        // calc as intersection of line from prev. position and current position with grid horizontal line
+                        var pixelBorderY = Math.floor(y / Generator.Parameters.CELL_SIZE) * Generator.Parameters.CELL_SIZE;
+                        var pixelBorderX = prevX + (x - prevX) * (pixelBorderY - prevY) / (y - prevY);
+                        leftCell = Math.floor((pixelBorderX - playerWidthHalf) / Generator.Parameters.CELL_SIZE);
+                        rightCell = Math.floor((pixelBorderX + playerWidthHalf) / Generator.Parameters.CELL_SIZE);
+                        // all cells in x direction occupied with body
+                        for (var i = leftCell; i <= rightCell; i++) {
+                            var visitedId = i + (cellY << 8);
+                            // if not already in list, then add new jump to reach this cell
+                            if (typeof visitedList[visitedId] === "undefined") {
+                                var jump = new Generator.Jump();
+                                jump.offsetX = i;
+                                jump.offsetY = cellY;
+                                jumpDefs.push(jump);
+                                console.log(jump.toString());
+                            }
+                        }
+                        // debug
+                        if (JumpTables._DEBUG) {
+                            // debug draw
+                            var py = pixelBorderY + yOffset;
+                            // line with original body width
+                            var color = "#4040FF";
+                            var pxLeft = pixelBorderX - Generator.Parameters.PLAYER_BODY_WIDTH / 2;
+                            var pxRight = pixelBorderX + Generator.Parameters.PLAYER_BODY_WIDTH / 2;
+                            debugBitmap.line(pxLeft, py, pxRight, py, color);
+                            color = "#0000FF";
+                            pxLeft = pixelBorderX - playerWidthHalf;
+                            pxRight = pixelBorderX + playerWidthHalf;
+                            // line with shortened body width
+                            debugBitmap.line(pxLeft, py, pxRight, py, color);
+                            debugBitmap.line(pxLeft, py - 3, pxLeft, py + 3, color);
+                            debugBitmap.line(pxRight, py - 3, pxRight, py + 3, color);
+                        }
+                    }
+                }
+                leftCell = Math.floor((x - playerWidthHalf) / Generator.Parameters.CELL_SIZE);
+                rightCell = Math.floor((x + playerWidthHalf) / Generator.Parameters.CELL_SIZE);
+                // add grid cells to visited
+                for (var i = leftCell; i <= rightCell; i++) {
+                    // make "id"
+                    var visitedId = i + (cellY << 8);
+                    if (typeof visitedList[visitedId] === "undefined") {
+                        visitedList[visitedId] = visitedId;
+                    }
+                }
+            }
+            this._jumpDefs[jumpIndex][step] = jumpDefs;
+        };
+        // -------------------------------------------------------------------------
+        JumpTables.prototype.analyzeJumpTables = function () {
+            // min y
+            this._jumpOffsetYMax = 0;
+            // through all jump velocities
+            for (var velocity = 0; velocity < this._jumpDefs.length; velocity++) {
+                // get only first x position within cell and first jump for given velocity,
+                // because all have the same height
+                this._jumpOffsetsY[velocity] = this._jumpDefs[velocity][0][0].offsetY;
+                // check for maximum offset in y direction.
+                // As it is negative number, we are looking for min in fact
+                this._jumpOffsetYMax = Math.min(this._jumpOffsetYMax, this._jumpOffsetsY[velocity]);
+            }
+            // find minimum and maximum offset in cells to jump to at given height level
+            for (var velocity = 1; velocity < this._jumpDefs.length; velocity++) {
+                // get only first startX, because it has smallest x offset
+                var jumps = this._jumpDefs[velocity][0];
+                for (var j = 0; j < jumps.length; j++) {
+                    var jump = jumps[j];
+                    var currentMin = this._jumpOffsetXMins[jump.offsetY];
+                    this._jumpOffsetXMins[jump.offsetY] = (typeof currentMin !== "undefined") ?
+                        Math.min(currentMin, jump.offsetX) : jump.offsetX;
+                    // console.log("LEVEL: " + jump.offsetY + " - jump from " + this.minOffsetX(jump.offsetY));
+                }
+                // get only last startX, because it has biggest x offset
+                jumps = this._jumpDefs[velocity][this._jumpDefs[velocity].length - 1];
+                for (var j = 0; j < jumps.length; j++) {
+                    var jump = jumps[j];
+                    var currentMax = this._jumpOffsetXMaxs[jump.offsetY];
+                    this._jumpOffsetXMaxs[jump.offsetY] = (typeof currentMax !== "undefined") ?
+                        Math.max(currentMax, jump.offsetX) : jump.offsetX;
+                    // console.log("LEVEL: " + jump.offsetY + " - jump to " + this.maxOffsetX(jump.offsetY));
+                }
+            }
+        };
+        // -------------------------------------------------------------------------
+        JumpTables.prototype.maxOffsetY = function (jumpIndex) {
+            if (jumpIndex === void 0) { jumpIndex = -1; }
+            if (jumpIndex === -1) {
+                return this._jumpOffsetYMax;
+            }
+            else {
+                return this._jumpOffsetsY[jumpIndex];
+            }
+        };
+        // -------------------------------------------------------------------------
+        JumpTables.prototype.maxOffsetX = function (offsetY) {
+            var maxX = this._jumpOffsetXMaxs[offsetY];
+            if (typeof maxX === "undefined") {
+                console.error("max X for offset y = " + offsetY + " does not exist");
+                maxX = 0;
+            }
+            return maxX;
+        };
+        // -------------------------------------------------------------------------
+        JumpTables.prototype.minOffsetX = function (offsetY) {
+            var minX = this._jumpOffsetXMins[offsetY];
+            if (typeof minX === "undefined") {
+                console.error("min X for offset y = " + offsetY + " does not exist");
+                minX = 0;
+            }
+            return minX;
+        };
+        // -------------------------------------------------------------------------
         JumpTables.setDebug = function (debug, gameGlobals) {
             JumpTables._DEBUG = debug;
             JumpTables._globals = gameGlobals;
@@ -98,12 +300,14 @@ var Generator;
             }
         };
         Object.defineProperty(JumpTables, "debugBitmapData", {
+            // -------------------------------------------------------------------------
             get: function () {
                 return JumpTables._debugBmd;
             },
             enumerable: false,
             configurable: true
         });
+        // -------------------------------------------------------------------------
         JumpTables.createDebugBitmap = function () {
             var global = JumpTables._globals;
             var bmd = new Phaser.BitmapData(global.game, "Grid", global.GAME_WIDTH, global.GAME_HEIGHT);
@@ -121,6 +325,9 @@ var Generator;
             JumpTables._debugBmd = bmd;
         };
         JumpTables._instance = null;
+        // -------------------------------------------------------------------------
+        // ------------------------------ DEBUG ------------------------------------
+        // -------------------------------------------------------------------------
         JumpTables._DEBUG = false;
         return JumpTables;
     }());
