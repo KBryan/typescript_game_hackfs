e56447f60341bb6fc8667cd7e45af2281346bda0
Kwame Bryan <kwame.bryan@gmail.com>
2021-08-02 21:02:40 -0400

    Added Jump Tables / Debug Grid for player and platform placement.

--- js/shadowysupercoder.js
+++ js/shadowysupercoder.js
@@ -43,6 +43,113 @@ var ShadowySuperCoder;
     }(Phaser.Game));
     ShadowySuperCoder.Game = Game;
 })(ShadowySuperCoder || (ShadowySuperCoder = {}));
+var Generator;
+(function (Generator) {
+    var JumpTables = /** @class */ (function () {
+        function JumpTables() {
+            // velocites
+            this._jumpVelocites = [];
+            this.calculateJumpVelocities();
+        }
+        Object.defineProperty(JumpTables, "instance", {
+            get: function () {
+                if (JumpTables._instance === null) {
+                    JumpTables._instance = new JumpTables();
+                }
+                return JumpTables._instance;
+            },
+            enumerable: false,
+            configurable: true
+        });
+        JumpTables.prototype.calculateJumpVelocities = function () {
+            // all height samples
+            for (var i = 0; i <= Generator.Parameters.HEIGHT_STEPS; i++) {
+                // maximun height of jump for this step
+                var height = Generator.Parameters.HEIGHT_MIN + (Generator.Parameters.HEIGHT_MAX - Generator.Parameters.HEIGHT_MIN) / Generator.Parameters.HEIGHT_STEPS * i;
+                // v - sqrt(-(2 * s * g))
+                this._jumpVelocites[i] = -Math.sqrt(2 * height * Generator.Parameters.GRAVITY);
+            }
+        };
+        Object.defineProperty(JumpTables.prototype, "minJumpVelocity", {
+            get: function () {
+                return this._jumpVelocites[0];
+            },
+            enumerable: false,
+            configurable: true
+        });
+        Object.defineProperty(JumpTables.prototype, "maxJumpVelocity", {
+            get: function () {
+                return this._jumpVelocites[this._jumpVelocites.length - 1];
+            },
+            enumerable: false,
+            configurable: true
+        });
+        JumpTables.setDebug = function (debug, gameGlobals) {
+            JumpTables._DEBUG = debug;
+            JumpTables._globals = gameGlobals;
+            if (debug) {
+                if (typeof gameGlobals === "undefined" || gameGlobals === null) {
+                    console.warn("No game globals provided - switching debug off");
+                    JumpTables._DEBUG = false;
+                }
+                else {
+                    JumpTables.createDebugBitmap();
+                }
+            }
+        };
+        Object.defineProperty(JumpTables, "debugBitmapData", {
+            get: function () {
+                return JumpTables._debugBmd;
+            },
+            enumerable: false,
+            configurable: true
+        });
+        JumpTables.createDebugBitmap = function () {
+            var global = JumpTables._globals;
+            var bmd = new Phaser.BitmapData(global.game, "Grid", global.GAME_WIDTH, global.GAME_HEIGHT);
+            bmd.fill(192, 192, 192);
+            // horizontal lines
+            for (var i = 0; i < global.GAME_HEIGHT; i += Generator.Parameters.CELL_SIZE) {
+                bmd.line(0, i + 0.5, global.GAME_WIDTH - 1, i + 0.5);
+            }
+            // vertical lines
+            for (var i = 0; i < global.GAME_WIDTH; i += Generator.Parameters.CELL_SIZE) {
+                bmd.line(i + 0.5, 0, i + 0.5, global.GAME_HEIGHT - 1);
+                // add columns header numbers
+                bmd.text("" + (i / Generator.Parameters.CELL_SIZE), i + 20, 20, "24px Courier", "#FFFF00");
+            }
+            JumpTables._debugBmd = bmd;
+        };
+        JumpTables._instance = null;
+        JumpTables._DEBUG = false;
+        return JumpTables;
+    }());
+    Generator.JumpTables = JumpTables;
+})(Generator || (Generator = {}));
+var Generator;
+(function (Generator) {
+    var Parameters = /** @class */ (function () {
+        function Parameters() {
+        }
+        // game area
+        Parameters.GRID_HEIGHT = 10;
+        Parameters.CELL_SIZE = 64;
+        Parameters.CELL_STEPS = 4;
+        // gravity
+        Parameters.GRAVITY = 2400;
+        // player body dimensions
+        Parameters.PLAYER_BODY_WIDTH = 30;
+        Parameters.PLAYER_BODY_HEIGHT = 90;
+        // jump height params
+        Parameters.HEIGHT_MIN = Parameters.CELL_SIZE * 0.75;
+        Parameters.HEIGHT_MAX = Parameters.CELL_SIZE * 2.90;
+        Parameters.HEIGHT_STEPS = 4;
+        // horizontal speed
+        Parameters.VELOCITY_X = 300;
+        return Parameters;
+    }());
+    Generator.Parameters = Parameters;
+})(Generator || (Generator = {}));
 var ShadowySuperCoder;
 (function (ShadowySuperCoder) {
     var Boot = /** @class */ (function (_super) {
@@ -65,7 +172,10 @@ var ShadowySuperCoder;
             return _super !== null && _super.apply(this, arguments) || this;
         }
         Play.prototype.create = function () {
-            this.stage.backgroundColor = 0x000000;
+            this.stage.backgroundColor = 0x80FF80;
+            Generator.JumpTables.setDebug(true, ShadowySuperCoder.Global);
+            Generator.JumpTables.instance;
+            this.game.add.sprite(0, 0, Generator.JumpTables.debugBitmapData);
         };
         Play.prototype.update = function () {
         };
--- src/States/Play.ts
+++ src/States/Play.ts
@@ -1,8 +1,10 @@
 namespace ShadowySuperCoder {
     export class Play extends Phaser.State {
         public create() {
-            this.stage.backgroundColor = 0x000000;
-
+            this.stage.backgroundColor = 0x80FF80;
+            Generator.JumpTables.setDebug(true, ShadowySuperCoder.Global);
+            Generator.JumpTables.instance;
+            this.game.add.sprite(0,0,Generator.JumpTables.debugBitmapData);
         }
 
         public update() {
